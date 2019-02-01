7221d7fd289bda1f81d47f3a539c911ccb2e80d6
Bogdan PC <vba@rams.colostate.edu>
2019-02-01 09:03:37 -0700

    PC commit

---
+++ LabOne.java
@@ -0,0 +1,12 @@
+/**
+ * Created by av on 23/01/2019.
+ */
+public class LabOne {
+
+
+    public static void main(String[] args )
+    {
+        System.out.print("Hello world");
+        System.out.println("foobar");
+    }
+}
---
+++ LabThree.java
@@ -0,0 +1,15 @@
+// (name_assignment) Assignment
+// Author: Bogdan A. Vasilchenko
+// Date:   01/28/2019
+// Class:  CS164
+// Email:  vba@cs.colostate.edu
+
+
+public class LabThree {
+
+    public static void main(String[] args){
+
+        System.out.println("text");
+
+    }
+}
