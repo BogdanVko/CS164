54ab6abe99b88f0f59fbf199202fcd4f906d8dcf
doom01net <beeer01net@gmail.com>
2019-03-05 10:18:59 -0700

    Addded P8

---
+++ P8/src/Transforms.java
@@ -0,0 +1,45 @@
+package P8.src;
+// P8 Assignment
+// Author: Bogdan A. Vasilchenko
+// Date:   03/05/2019
+// Class:  CS164
+// Email:  vba@cs.colostate.edu
+
+
+public class Transforms implements ImageInterface {
+
+    @Override
+    public void readImage(String inFile) {
+
+    }
+
+    @Override
+    public void writeImage(String outFile) {
+
+    }
+
+    @Override
+    public int[][] imageData() {
+        return new int[0][];
+    }
+
+    @Override
+    public void decode() {
+
+    }
+
+    @Override
+    public void swap() {
+
+    }
+
+    @Override
+    public void mirror() {
+
+    }
+
+    @Override
+    public void exchange() {
+
+    }
+}
