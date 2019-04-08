0dc6122d7f1041ddf7f72844d73e983657997c04
Bogdan PC <vba@rams.colostate.edu>
2019-04-08 17:20:59 -0600

    P9 done

---
+++ Miscellaneous/Fun.java
@@ -0,0 +1,23 @@
+package Miscellaneous;
+// Miscellaneous
+// Author: Bogdan A. Vasilchenko
+// Date:   4/1/2019
+// Class:  CS164
+// Email:  vba@cs.colostate.edu
+
+
+import javax.swing.*;
+public class Fun {
+
+    public static void main (String[] args){
+        JFrame frame = new JFrame();
+        JButton b = new JButton("Hello");
+
+
+        frame.setSize(400,500);
+        frame.setVisible(true);
+
+
+
+    }
+}
