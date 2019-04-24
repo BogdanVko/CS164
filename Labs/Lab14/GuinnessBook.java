22d7bb485e24f8632798a7dcc852eb05132dd4b8
Bogdan PC <vba@rams.colostate.edu>
2019-04-24 12:36:08 -0600

    rebuild and R17

---
+++ Labs/Lab14/GuinnessBook.java
@@ -0,0 +1,84 @@
+package Labs.Lab14;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class GuinnessBook {
+
+	private ArrayList<Animal> landAnimals;
+
+	public GuinnessBook(String filename) throws FileNotFoundException{
+
+		landAnimals = new ArrayList<Animal>();
+
+		File animalFile = new File(filename);
+		Scanner scan = new Scanner(animalFile);
+		
+		while(scan.hasNextLine()){
+			String[] animalInfo = scan.nextLine().split(" ");
+			String animalName = "";
+			
+			for(int i = 0; i < (animalInfo.length-1); i++){
+				animalName += animalInfo[i] + " ";
+			}
+			animalName = animalName.trim();
+			Integer topSpeed = Integer.parseInt(animalInfo[animalInfo.length-1]);
+
+			//insert the animal into the arraylist here, don't change any of the above code
+			Animal a = new Animal(animalName, topSpeed);
+			landAnimals.add(a);
+			
+			
+			
+			
+		}
+		scan.close();
+}
+
+
+	public String toString(){
+		String returnStr = "";
+		for(int i = 0; i < landAnimals.size(); i++) {
+			returnStr+= landAnimals.get(i).toString() + "\n";
+			
+		}
+		return returnStr;
+	}
+
+	private void testGuinnessBook() throws FileNotFoundException {
+
+        // uncomment the println statements to  find animals whose speed is within
+        // 2mph of a given speed
+
+         System.out.println("Is there an animal whose speed is around 70mph? " + landAnimals.contains(new Animal("fast animal", 70)));
+         System.out.println("Is there an animal whose speed is around 35mph? " + landAnimals.contains(new Animal("slow animal", 35)));
+         System.out.println();
+            
+        // The success of the above two statements depends on correct implementation of an equals method in the Animal class
+        // TAs:  please explain this point at some point during the recitation.
+            
+        //If we would like to know which animal it is:
+         System.out.println(landAnimals.get(landAnimals.indexOf(new Animal("fast animal", 70))));
+         System.out.println(landAnimals.get(landAnimals.indexOf(new Animal("slow animal", 35))));
+         System.out.println();
+
+        //Now print all the animals whose speed is around 50mph
+    }
+
+    public static void main(String args[]) throws FileNotFoundException {
+
+        // Once Animal.java is completed and GuinnessBook has a working toString,
+        // uncomment the next two lines to test the constructor and toString
+         GuinnessBook records = new GuinnessBook(args[0]);
+        // System.out.println(records);
+
+        // Next, uncomment the following line and lines from testGuinnessBook to test your
+        // Animal class equals method
+         records.testGuinnessBook();
+    }
+
+}
+
---
+++ Labs/Lab16/R16.java
@@ -0,0 +1,50 @@
+package Labs.Lab16;
+// R16      Assignment
+// Author : Bogdan A Vasilchenko
+// Date   : Feb 27, 2019
+// Class  : CS164
+// Email  : vba@cs.colostate.edu
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class R16{
+    ArrayList<String> words = new ArrayList<>();
+    
+    public static void readFile( String inputFile ) throws IOException {
+
+        File file = new File(inputFile);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (sc.hasNextLine()) {
+            Scanner s2 = new Scanner(sc.nextLine());
+            while (s2.hasNext()) {
+                String s = s2.next();
+                String type = s2.next().getClass().getSimpleName();
+                System.out.println(type + ": " + s); // doesn't work right
+
+            }
+        }
+    }
+
+    public static void writeFile( String outputFile ) throws IOException{
+        PrintWriter writer = new PrintWriter(outputFile);
+
+
+    }
+    
+    
+
+    
+    public static void main(String[] args) throws IOException {
+        readFile(args[0]);
+    }
+}
